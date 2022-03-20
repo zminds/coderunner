38c892cb9767c51d3f5544fc959c1265db52249a
zhangmin <zhangmin@sensetime.com>
2022-03-20 23:43:34 +0800

    remove  binary

--- go/unittest/gotest.go
+++ go/unittest/gotest.go
@@ -3,9 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+	fmt.Println(NewTest("hello world").name)
+	fmt.Println(NewTest("hello world").string())
 }
 
 func myFunc(param string) error {
 	return nil
+}
+
+type Test struct {
+	name string
+}
+
+func (t *Test) string() string {
+	return t.name
+}
+
+func NewTest(param string) (test *Test) {
+	return &Test{
+		name: param,
+	}
 }
--- go/unittest/gotest_test.go
+++ go/unittest/gotest_test.go
@@ -6,7 +6,7 @@ func Test_main(t *testing.T) {
 	tests := []struct {
 		name string
 	}{
-		// TODO: Add test cases.
+		Println("go test run")
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
